fb973601b14a4454be2c3fefd7f88a59efccd9d4
Dalia Alawneh <alawnehdalia340@gmail.com>
2023-08-07 20:31:28 +0300

    Implement get certificate functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,11 @@ const quizeCard = document.getElementById('quize-card')
 let lastQuestionReached = 0
 let currentQuestion = 0
 let score = 0
-
-
+const userAnswers = []
 function displayQuestion() {
     quizeCard.innerHTML = `<div class="title bg-peimery flex-centered between">
     <div>
-        <p class="question-num">Question number ${currentQuestion}</p>
+        <p class="question-num">Question number ${currentQuestion + 1} of ${questions.length}</p>
         <p id='score'>score: ${score}</p>
     </div>
     <div class="timer" id="timer">
@@ -17,32 +16,38 @@ function displayQuestion() {
     <div class="question">
         ${questions[currentQuestion].title}
     </div>
-    
     ${questions[currentQuestion].code ? `<div class="code">
         ${questions[currentQuestion].code}
     </div>`: ''}
-    
     <div class="options flex">
         ${questions[currentQuestion].options.map((option) => {
         return `<button class="outlined">${option}</button>`
     }).join('')}
-    </div>
-`
+    </div>`
+    const options = document.querySelectorAll('.options button')
+    options.forEach((option) => {
+        option.addEventListener('click', () => {
+            options.forEach(option => option.classList.remove('checked'))
+            option.classList.add('checked')
+        })
+    })
 }
-let timerFlag = false;
+
 displayQuestion()
+
+let timerFlag = false;
 const timer = document.getElementById('timer');
 let time = 60;
-const updateTimer = () => {
+function startTimer() {
     let timeer = setInterval(() => {
         if (timerFlag) {
-            clearInterval(timer)
+            console.log(time, timerFlag);
+            clearInterval(timeer)
             return
         }
         time -= 1
-        timer.innerHTML = time;
-        if (time == 0 || timerFlag) {
-            console.log(timerFlag)
+        document.getElementById('timer').innerHTML = `${time}`;
+        if (time <= 0) {
             clearInterval(timeer)
             Swal.fire({
                 title: '<strong>TimeOut</strong>',
@@ -55,20 +60,9 @@ const updateTimer = () => {
     }, 1000)
 };
 
-updateTimer()
+startTimer()
 
-
-
-const options = document.querySelectorAll('.options button')
-
-options.forEach((option) => {
-    option.addEventListener('click', () => {
-        options.forEach(option => option.classList.remove('checked'))
-        option.classList.add('checked')
-    })
-})
 const submitBtn = document.getElementById('submit')
-
 const checkAnswer = () => {
     const userAnswerElement = document.querySelector('.checked');
     const selectedAnswer = userAnswerElement ? userAnswerElement.textContent : null;
@@ -96,8 +90,10 @@ const checkAnswer = () => {
             } else {
                 score += 5
             }
-            submitBtn.innerHTML = 'Next <i class="fa-solid fa-angle-right"></i>'
+            document.getElementById('score').innerHTML =  `score: ${score}`
             timerFlag = true;
+            submitBtn.innerHTML = 'Next <i class="fa-solid fa-angle-right"></i>'
+            submitBtn.onclick = goToNextQuestion
         } else {
             userAnswerElement.classList.add('invalid');
             const allOptions = document.querySelectorAll('.options button');
@@ -107,8 +103,69 @@ const checkAnswer = () => {
                     option.classList.add('valid');
                 }
             });
+            Swal.fire({
+                title: '<strong>Wrong Answer</strong>',
+                icon: 'error',
+                showCloseButton: true,
+                focusConfirm: false
+            });
+            timerFlag = true;
+            submitBtn.innerHTML = 'Next <i class="fa-solid fa-angle-right"></i>'
+            submitBtn.onclick = goToNextQuestion
         }
     }
 };
 
 submitBtn.onclick = checkAnswer
+
+const goToNextQuestion = () => {
+    currentQuestion++
+    if (currentQuestion === questions.length) {
+        Swal.fire({
+            title: '<strong>You have done all qestions</strong>',
+            icon: 'warning',
+            showCloseButton: true,
+            focusConfirm: false
+        });
+        submitBtn.innerHTML = 'Get Certified <i class="fa fa-certificate"></i>'
+        submitBtn.onclick = checkResult
+        return
+    }
+    submitBtn.innerHTML = 'check<i class="fa-solid fa-check-double"></i>'
+    submitBtn.onclick = checkAnswer
+    timerFlag = false
+    displayQuestion()
+    time = 60;
+    startTimer()
+
+}
+const goToPrevQuestion = () => {
+    currentQuestion--
+    // if(currentQuestion<=0){
+    //     Swal.fire({
+    //         title: "<strong>You can't go back this is the first question</strong>",
+    //         icon: 'error',
+    //         showCloseButton: true,
+    //         focusConfirm: false
+    //     });
+
+    //     return
+    // }
+    displayQuestion()
+}
+
+document.getElementById('back-btn').onclick = goToPrevQuestion
+
+const checkResult = () => {
+    if(score >= 25){
+        window.location.href = 'social-share.html'
+    }else{
+        Swal.fire({
+            title: '<strong>Faild to pass test</strong>',
+            htel: `<p>Your score is ${score} which is low so try again to pass the test</p>`,
+            icon: 'error',
+            showCloseButton: true,
+            focusConfirm: false
+        });
+    }
+}
